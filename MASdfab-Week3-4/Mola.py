013b91eb20b3623a5d4fbe68f0e799f7fb62a39e
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2022-10-11 16:56:08 +0200

    On branch main 	modified:   MASdfab-Week3-4.md 	new file:   MASdfab-Week3-4/MASdfab-Week3-4_2022-10-11-14-31-58.png 	new file:   MASdfab-Week3-4/MASdfab-Week3-4_2022-10-11-16-54-33.png 	modified:   MASdfab-Week3-4/Mola.gh 	modified:   MASdfab-Week3-4/Mola.py

--- MASdfab-Week3-4/Mola.py
+++ MASdfab-Week3-4/Mola.py
@@ -1,21 +1,79 @@
 import mola
-import math
+import random
 import Rhino.Geometry as rg
 
-molaMesh = mola.construct_sphere(radius=5, u_res=8, v_res=8)
+mMesh = mola.construct_single_face(
+    [mola.Vertex(0, 0, 0), mola.Vertex(10, 0, 0), mola.Vertex(10, 10, 0), mola.Vertex(0, 10, 0)]
+)
 
-for v in molaMesh.vertices:
-    if v.z > 0:
-        v.z *= 1.8
+newMesh = mola.Mesh()
 
-molaMesh = mola.subdivide_mesh_catmull(molaMesh)
+for face in mMesh.faces:
+    newFaces = mola.subdivide_face_extrude_to_point_center(face, 0)
+    newMesh.faces.extend(newFaces)
 
-for f in molaMesh.faces:
-    molaMesh.faces.extend(mola.subdivide_face_extrude_tapered(f, 10,0.5, True))
-    
-    
-Mesh = mola.module_rhino.display_mesh(molaMesh)
+mMesh = newMesh
+mMesh.update_topology()
+mMesh = mola.subdivide_mesh_catmull(mMesh)
 
-# outTemp = []
-# for face in molaMesh.faces:
-    # outTemp.append(rg.Point3d(mola.face_center(face).x, mola.face_center(face).y, mola.face_center(face).z))
+newMesh = mola.Mesh()
+for face in mMesh.faces:
+    newFaces = mola.subdivide_face_split_grid(face, 2, 1)
+    newMesh.faces.extend(newFaces)
+
+mMesh = newMesh
+
+
+for face in mMesh.faces:
+    face.group = "plot"
+
+newMesh = mola.Mesh()
+for f in mMesh.faces:
+    if f.group == "plot":
+        new_faces = mola.subdivide_face_extrude_tapered(f, 0, 0.3, True)
+        for nf in new_faces[:-1]:
+            nf.group = "circulation"
+            nf.color = (1, 0, 0)
+        # [-1] is take the last element [:-1] is for taking all elements
+        new_faces[-1].group = "construction"
+        new_faces[-1].color = (0.5, 0, 1)
+        newMesh.faces.extend(new_faces)
+    else:
+        newMesh.faces.append(f)
+
+
+mMesh = newMesh
+
+########################################################################
+
+
+newMesh = mola.Mesh()
+for face in mMesh.faces:
+    if face.group == "construction":
+        if random.random() < 0.2:
+            newFaces = mola.subdivide_face_extrude_to_point_center(face, 1)
+            for newFace in newFaces:
+                newFace.group = "park"
+                newFace.color = (0, 1, 0)
+            newMesh.faces.extend(newFaces)
+        else:
+            floorNumber = random.randint(5, 20)
+            buildingSurface = [face]
+            for i in range(floorNumber):
+                buildingSurface.extend(mola.subdivide_face_extrude(buildingSurface[-1], 0.1, False))
+                buildingSurface.pop(-6)
+            for newFace in buildingSurface:
+                newFace.group = "building"
+                newFace.color = (0, 0, 1)
+            newMesh.faces.extend(buildingSurface)
+
+
+    newMesh.faces.append(face)
+
+newMesh.update_topology()
+newMesh = mola.subdivide_mesh_catmull(newMesh)
+newMesh = mola.subdivide_mesh_catmull(newMesh)
+
+mMesh = newMesh
+
+Mesh = mola.module_rhino.display_mesh(mMesh)
